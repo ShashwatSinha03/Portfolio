aee99afc71eb6f926738bd76198d0c5d899e7fa2
Shashwat Sinha <76537375+ShashwatSinha03@users.noreply.github.com>
2026-07-04 07:45:56 +0530

    Make 'that' white in hero tagline

--- app/(home)/_components/Hero.tsx
+++ app/(home)/_components/Hero.tsx
@@ -59,7 +59,7 @@ export default function Hero() {
         >
           Building
           <br />
-          <span className="text-[var(--color-fg-tertiary)]">system that</span>
+          <span className="text-[var(--color-fg-tertiary)]">system </span><span className="text-[var(--color-fg-primary)]">that</span>
           <br />
           <span className="relative inline-block text-[var(--color-fg-tertiary)]">
             last.
