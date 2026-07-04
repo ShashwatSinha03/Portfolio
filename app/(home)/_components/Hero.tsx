3a2a9105160ac3f090911d5a56c37b416783fb28
Shashwat Sinha <76537375+ShashwatSinha03@users.noreply.github.com>
2026-07-04 07:44:31 +0530

    Set 'last.' in Tenor Sans (font-secondary)

--- app/(home)/_components/Hero.tsx
+++ app/(home)/_components/Hero.tsx
@@ -61,7 +61,7 @@ export default function Hero() {
           <br />
           <span className="text-[var(--color-fg-tertiary)]">system that</span>
           <br />
-          <span className="relative inline-block">
+          <span className="relative inline-block font-secondary">
             last.
             <span className="absolute -bottom-2 left-0 h-px w-full bg-[var(--color-fg-primary)]/20" />
           </span>
